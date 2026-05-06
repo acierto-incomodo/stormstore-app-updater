2292f1bee2187a305adf2249b1747dd346f7d43d
acierto-incomodo <97026099+acierto-incomodo@users.noreply.github.com>
2026-05-06 11:39:08 +0200

    Act 30

--- format.ps1
+++ format.ps1
@@ -1,5 +1,7 @@
 # Reemplazar espacios por guiones en los nombres de archivo .exe y .blockmap generados
-Get-ChildItem -Path . -Recurse -Include '*.exe', '*.blockmap' | ForEach-Object {
-    $newName = $_.Name -replace ' ', '-'
-    Rename-Item -Path $_.FullName -NewName $newName
+Get-ChildItem -Path "dist" -Recurse -Include '*.exe', '*.blockmap' -ErrorAction SilentlyContinue | ForEach-Object {
+    if ($_.Name -match ' ') {
+        $newName = $_.Name -replace ' ', '-'
+        Rename-Item -Path $_.FullName -NewName $newName
+    }
 }
