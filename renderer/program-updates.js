c29d4970f95159ae70515ac95635202ed2687dbc
acierto-incomodo <97026099+acierto-incomodo@users.noreply.github.com>
2026-05-21 16:28:59 +0200

    Act 75
    Agregado "Backrooms: Escape Together" + cambios a la hora de instalar/actualizar mediante files.apps.json

--- renderer/program-updates.js
+++ renderer/program-updates.js
@@ -287,9 +287,11 @@ document.addEventListener("DOMContentLoaded", async () => {
           });
         } else {
           setStatus("Todas las actualizaciones completadas.");
+          window.api.clearDownloads();
           disableNavigation(false);
         }
       } else {
+        window.api.clearDownloads();
         disableNavigation(false);
       }
     } else {
